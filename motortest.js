a37755d84296ccef4b5d45c9c6f93c380ccc1e3a
lopfi95 <lopfi95@gmail.com>
2020-01-24 16:03:15 +0100

    added motor test script

---
+++ motortest.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const Gpio = require('../').Gpio;
+
+const motor_r = new Gpio(03, {mode: Gpio.OUTPUT});
+
+let dutyCycle = 0;
+
+setInterval(() => {
+  motor_r.pwmWrite(dutyCycle);
+
+  dutyCycle += 5;
+  if (dutyCycle > 255) {
+    dutyCycle = 0;
+  }
+}, 20);
