c378de5dfbd58f6495b767e17e7ebb3caf4eb1e5
lopfi95 <lopfi95@gmail.com>
2020-01-25 18:14:18 +0100

    c

--- motortest2.js
+++ motortest2.js
@@ -9,12 +9,12 @@ const enable_l = new Gpio(07, {mode: Gpio.OUTPUT});
 
 
 while(true){
-enable_r.pwmWrite(100);
-enable_l.pwmWrite(100);
-//enable_r.digitalWrite(1);
-//enable_l.digitalWrite(0);
-motor_r1.digitalWrite(1);
+//enable_r.pwmWrite(100);
+//enable_l.pwmWrite(100);
+enable_r.digitalWrite(1);
+enable_l.digitalWrite(1);
+motor_r1.pwmWrite(100);
 motor_r2.digitalWrite(0);
-motor_l1.digitalWrite(1);
+motor_l1.pwmWrite(100);
 motor_l2.digitalWrite(0);
 }
