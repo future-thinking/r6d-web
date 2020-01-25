c7bbb2eda281e6006478e58044481e1ce63622b5
P3ntest <juliusvanvoorden@outlook.com>
2020-01-25 17:12:45 +0100

    add logtest.js

---
+++ logtest.js
@@ -0,0 +1,15 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.send("<script src='/socket.io/socket.io.js'></script><script>var socket = io();</script>");
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
