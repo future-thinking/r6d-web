5ecfcde655b552a1a67cee038d24a8429e4ce7ab
P3ntest <juliusvanvoorden@outlook.com>
2020-01-25 14:34:23 +0100

    remove opencv integration

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-const cv = require('opencv');
+//const cv = require('opencv');
 const Gpio = require('pigpio').Gpio;
 
 const motor_r = new Gpio(03, {mode: Gpio.OUTPUT});
