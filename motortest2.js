7af6a3ae6147983610c4fb04bc5fa8547bae7e45
lopfi95 <lopfi95@gmail.com>
2020-01-25 13:38:17 +0100

    debug

--- motortest2.js
+++ motortest2.js
@@ -4,6 +4,7 @@ const motor_r1 = new Gpio(02, {mode: Gpio.OUTPUT});
 const motor_r2 = new Gpio(03, {mode: Gpio.OUTPUT});
 
 let dutyCycle = 255;
-
+while (true){
 motor_r1.digitalWrite(1);
 motor_r2.digitalWrite(1);
+}
