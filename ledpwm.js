f5d79d63ba1ad0cfbad3583a18bd6cfe99dace0a
lopfi95 <lopfi95@gmail.com>
2020-01-25 19:14:00 +0100

    changed led pwm test script

--- ledpwm.js
+++ ledpwm.js
@@ -2,13 +2,4 @@ const Gpio = require('pigpio').Gpio;
 
 const led = new Gpio(17, {mode: Gpio.OUTPUT});
 
-let dutyCycle = 0;
-
-setInterval(() => {
-  led.pwmWrite(dutyCycle);
-
-  dutyCycle += 5;
-  if (dutyCycle > 255) {
-    dutyCycle = 0;
-  }
-}, 20);
+  led.pwmWrite(50);
