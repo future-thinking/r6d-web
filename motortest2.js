bd165784a34f1e26603e53e0c0c4d56c898775b0
lopfi95 <lopfi95@gmail.com>
2020-01-25 20:37:23 +0100

    hopefully fixed motortest2

--- motortest2.js
+++ motortest2.js
@@ -1,20 +1,18 @@
 const Gpio = require('pigpio').Gpio;
 
-const motor_r1 = new Gpio(06, {mode: Gpio.OUTPUT});
+const motor_r1 = new Gpio(14, {mode: Gpio.OUTPUT});
 const motor_r2 = new Gpio(15, {mode: Gpio.OUTPUT});
-const enable_r = new Gpio(14, {mode: Gpio.OUTPUT});
-const motor_l1 = new Gpio(07, {mode: Gpio.OUTPUT});
+const enable_r = new Gpio(18, {mode: Gpio.OUTPUT});
+const motor_l1 = new Gpio(04, {mode: Gpio.OUTPUT});
 const motor_l2 = new Gpio(03, {mode: Gpio.OUTPUT});
 const enable_l = new Gpio(02, {mode: Gpio.OUTPUT});
 
 
 while(true){
-//enable_r.pwmWrite(100);
-//enable_l.pwmWrite(100);
-enable_r.digitalWrite(1);
-enable_l.digitalWrite(1);
-motor_r1.pwmWrite(100);
+enable_r.pwmWrite(170);
+enable_l.pwmWrite(170);
+motor_r1.digitalWrite(1);
+motor_l1.digitalWrite(1);
 motor_r2.digitalWrite(0);
-motor_l1.pwmWrite(100);
 motor_l2.digitalWrite(0);
 }
