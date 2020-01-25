37655d9bd579c5d3818c21e2669ed0e783483269
RobinD95 <lopfi95@gmail.com>
2020-01-25 14:46:29 +0100

    asdfsa

--- motortest2.js
+++ motortest2.js
@@ -1,6 +1,6 @@
 const Gpio = require('pigpio').Gpio;
 
-const motor_r1 = new Gpio(02, {mode: Gpio.OUTPUT});
+const motor_r1 = new Gpio(07, {mode: Gpio.OUTPUT});
 const motor_r2 = new Gpio(03, {mode: Gpio.OUTPUT});
 
 let dutyCycle = 255;
