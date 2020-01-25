a8391e4e1a7ed50b4b75693b6379ad72f897c485
lopfi95 <lopfi95@gmail.com>
2020-01-25 17:48:01 +0100

    sdf

--- motortest2.js
+++ motortest2.js
@@ -7,9 +7,12 @@ const motor_l1 = new Gpio(03, {mode: Gpio.OUTPUT});
 const motor_l2 = new Gpio(05, {mode: Gpio.OUTPUT});
 const enable_l = new Gpio(07, {mode: Gpio.OUTPUT});
 
+
+while(true){
 enable_r.pwmWrite(255);
 enable_l.pwmWrite(255);
 motor_r1.digitalWrite(1);
 motor_r2.digitalWrite(0);
 motor_l1.digitalWrite(1);
 motor_l2.digitalWrite(0);
+}
