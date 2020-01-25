fbd8589876e7187c0d3635fa2afe97db919a4006
lopfi95 <lopfi95@gmail.com>
2020-01-25 19:26:11 +0100

    edited led pwm

--- ledpwm.js
+++ ledpwm.js
@@ -1,6 +1,8 @@
 const Gpio = require('pigpio').Gpio;
 const led = new Gpio(17, {mode: Gpio.OUTPUT});
+const forward = new Gpio(27, {mode: Gpio.OUTPUT});
 
 while (true) {
   led.pwmWrite(50);
+  forward.digitalWrite(1);
 }
