3885897cb04bff4e92704a974522790eb05e7679
lopfi95 <lopfi95@gmail.com>
2020-01-25 17:45:08 +0100

    updated motortest2

--- motortest2.js
+++ motortest2.js
@@ -1,31 +1,15 @@
-async function init() {
-  console.log(1);
-  await sleep(1000);
-  console.log(2);
-}
-
-function sleep(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-}
-
 const Gpio = require('pigpio').Gpio;
 
-const motor_r1 = new Gpio(03, {mode: Gpio.OUTPUT});
-const motor_r2 = new Gpio(05, {mode: Gpio.OUTPUT});
-const enable_r = new Gpio(07, {mode: Gpio.OUTPUT});
+const motor_r1 = new Gpio(08, {mode: Gpio.OUTPUT});
+const motor_r2 = new Gpio(10, {mode: Gpio.OUTPUT});
+const enable_r = new Gpio(12, {mode: Gpio.OUTPUT});
 const motor_l1 = new Gpio(03, {mode: Gpio.OUTPUT});
 const motor_l2 = new Gpio(05, {mode: Gpio.OUTPUT});
 const enable_l = new Gpio(07, {mode: Gpio.OUTPUT});
-var i = 0;
 
-enable_r.digitalWrite(1);
-enable_r.digitalWrite(1);
-
-while (i < 4){
+enable_r.pwmWrite(255);
+enable_l.pwmWrite(255);
 motor_r1.digitalWrite(1);
 motor_r2.digitalWrite(0);
-sleep(2000);
-i++;
-}
+motor_l1.digitalWrite(1);
+motor_l2.digitalWrite(0);
